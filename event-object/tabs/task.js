3d509ce824e9ae6a44049d66f12b9d95a199dadb
SBoyarkin <sergey.boyarkin@mail.ru>
2025-02-06 00:19:01 +0500

    complet tabs

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -4,14 +4,12 @@ console.log(tabs)
 console.log(tabs_content)
 
 const change_tabs = function (index) {
-    tabs_content.forEach((value, i, array) => {i == index
+    tabs_content.forEach((value, i, array) => { i == index
         ? value.classList.add('tab__content_active')
-        : value.classList.remove('tab__content_active')})
-
-}
+        : value.classList.remove('tab__content_active')})}
 
 tabs.forEach((value, index, array)=> value.addEventListener('click',
-    ()=> array.forEach((el, idx) => {idx == index
+    ()=> array.forEach((el, idx) => { idx == index
         ? (el.classList.add('tab_active'), change_tabs(index))
         : el.classList.remove('tab_active')})))
 
