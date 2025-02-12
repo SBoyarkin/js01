b76cc54baa7497d2d0f9df0b5b03f287468c21ed
SBoyarkin <sergey.boyarkin@mail.ru>
2025-02-13 00:30:18 +0500

    compleet tooltip

---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,31 @@
+const has_tooltip = [...document.querySelectorAll('.has-tooltip')]
+
+const create_tooltip = function(text) {
+    let tooltip = document.createElement('div')
+    tooltip.classList.add('tooltip')
+    tooltip.classList.add('tooltip_active')
+    tooltip.innerText = text
+    return tooltip
+
+}
+
+const open_tooltip = function (event) {
+    event.preventDefault()
+    active_tooltip = document.querySelector('.tooltip')
+    if (active_tooltip === null) {
+       let obj = create_tooltip(event.target.getAttribute('title'))
+        event.target.appendChild(obj)
+
+    } else {
+        if (event.target.children.length > 0) {
+            event.target.removeChild(active_tooltip)
+        }
+        else {
+            active_tooltip.parentElement.removeChild(active_tooltip)
+            let obj = create_tooltip(event.target.getAttribute('title'))
+        event.target.appendChild(obj)
+        }
+    }
+}
+
+has_tooltip.forEach((value) => value.addEventListener('click', open_tooltip))
