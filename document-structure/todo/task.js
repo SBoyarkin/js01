7924595f9b3642e30462939cb1ae38f5874dd2f0
SBoyarkin <sergey.boyarkin@mail.ru>
2025-02-16 00:14:16 +0500

    16.02 commit? complete todo

---
+++ document-structure/todo/task.js
@@ -0,0 +1,24 @@
+const todo_form = document.getElementById('tasks__form')
+const input_data = document.getElementById('task__input')
+const task_list = document.getElementById('tasks__list')
+
+
+
+const add_event = function (e) {
+    e.preventDefault()
+    if (input_data.value.length > 0) {
+        let new_el = document.createElement("div")
+        new_el.innerHTML =
+            `
+                    <div class="task__title">
+                    ${input_data.value}
+                    </div>
+                    <a href="#" class="task__remove">&times;</a>
+            `
+        new_el.classList = 'task'
+        const remover = new_el.querySelector('.task__remove')
+        remover.addEventListener('click', (e) => {e.target.parentElement.remove()})
+        task_list.appendChild(new_el)
+    }
+}
+todo_form.addEventListener('submit', add_event)
--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,9 +1,10 @@
 const has_tooltip = [...document.querySelectorAll('.has-tooltip')]
 
-const create_tooltip = function(text) {
+const create_tooltip = function(text, x) {
     let tooltip = document.createElement('div')
     tooltip.classList.add('tooltip')
     tooltip.classList.add('tooltip_active')
+    tooltip.style.left = `${x}px`
     tooltip.innerText = text
     return tooltip
 
@@ -11,18 +12,18 @@ const create_tooltip = function(text) {
 
 const open_tooltip = function (event) {
     event.preventDefault()
+     let el = create_tooltip(event.target.getAttribute('title'), event.target.offsetLeft,)
     active_tooltip = document.querySelector('.tooltip')
     if (active_tooltip === null) {
-       let obj = create_tooltip(event.target.getAttribute('title'))
+       let obj = el
         event.target.appendChild(obj)
-
     } else {
         if (event.target.children.length > 0) {
             event.target.removeChild(active_tooltip)
         }
         else {
             active_tooltip.parentElement.removeChild(active_tooltip)
-            let obj = create_tooltip(event.target.getAttribute('title'))
+            let obj = el
         event.target.appendChild(obj)
         }
     }
