dea657affad3eb751302508db553889f6509b4c9
SBoyarkin <sergey.boyarkin@mail.ru>
2025-02-17 22:02:48 +0500

    cart complete

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -3,25 +3,21 @@ const control_inc = [...document.querySelectorAll('.product__quantity-control_in
 const product_add = [...document.querySelectorAll('.product__add')]
 const cart_products = document.querySelector('.cart__products')
 
-
 const add_cart_product = function (element) {
     const count_cart = cart_products.querySelectorAll('.cart__product')
     if (!count_cart.length) {
         cart_products.appendChild(element)
     } else
     {
-
         const in_cart = [...count_cart].findIndex((value)=> { return value.dataset.id == element.dataset.id} )
         if (in_cart == -1) {
             cart_products.appendChild(element)
         }
         else {
-            console.log(count_cart.item(in_cart))
-            // console.log(count_cart.item(in_cart).textContent = count_cart.item(in_cart).querySelector('cart__product-count'))
+            let current_count = count_cart.item(in_cart).querySelector('.cart__product-count')
+            current_count.textContent = Number(current_count.textContent) + Number(element.querySelector('.cart__product-count').textContent)
         }
-
     }
-
 }
 
 const dec = function (event) {
@@ -31,7 +27,6 @@ const dec = function (event) {
         cur_val -= 1
         event.target.nextElementSibling.textContent = cur_val
     }
-
 }
 
 const inc = function (event) {
@@ -41,13 +36,10 @@ const inc = function (event) {
     event.target.previousElementSibling.textContent = cur_val
 }
 
-
 const append_products = function (event) {
         let product = event.currentTarget.closest('.product')
         let url = product.querySelector('.product__image')
         url.getAttribute('href')
-
-
         let cp = document.createElement("div")
         let a = document.createElement('img')
         let product_count = document.createElement('div')
@@ -55,7 +47,6 @@ const append_products = function (event) {
         cp.dataset.id = product.dataset.id
         a.classList.add('cart__product-image')
         a.setAttribute('src', url.src)
-
     product_count.classList.add('cart__product-count')
     product_count.textContent = event.currentTarget.previousElementSibling.querySelector('.product__quantity-value').textContent // добавить сюда колличество
     cp.appendChild(a)
